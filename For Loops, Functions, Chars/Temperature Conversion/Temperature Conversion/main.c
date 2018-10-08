c3f8dea0e70cccbdcf01ceeeccf8f2cd968e6ae8
Ryan Yun <yunr@oregonstate.edu>
2018-10-09 03:47:14 +0900

    Temperature Conversion Program

---
+++ For Loops, Functions, Chars/Temperature Conversion/Temperature Conversion/main.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+main()
+{
+	float fahr, cels; // Float to hold fahrenheit and celsius
+	int lowerlimit = 0; // Lower limit bound
+	int upperlimit = 300; // Upper limit bound
+	int step = 20; // Print in intervals of twenty
+
+	fahr = lowerlimit; // Set fahr to lowerlimit
+	printf("Temperature Conversion Table\n\n");
+	printf("Fahrenheit to Celsius\n");
+	while (fahr <= upperlimit) // Loop while fahrenheit is lower than or equal toupper limit 
+	{
+		cels = (5.0 / 9.0) * (fahr - 32.0);
+		printf("%3.0f        %6.1f\n", fahr, cels);
+
+		fahr = fahr + step;
+	}
+
+	cels = 0; // Reset cels to zero
+	printf("\n");
+	printf("Celsius to Fahrenheit\n"); // Print Celsius to fahrenheit conversion
+	while (cels <= upperlimit)
+	{
+		fahr = ((9.0/5.0) * (cels)) + (32);
+		printf("%6.1f        %3.0f\n", cels, fahr);
+
+		cels = cels + step;
+	}
+
+	system("pause");
+	return 0;
+}
